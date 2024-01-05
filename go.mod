873c970a19d55f4df6bbfe6fd44b9b51ee474dee
Ignas Bernotas <ignas@iber.lt>
2024-01-06 01:19:35 +0200

    feat: logging config

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-git/go-git/v5 v5.6.1
 	github.com/google/go-github/v28 v28.1.1
+	github.com/google/uuid v1.5.0
 	github.com/joho/godotenv v1.5.1
 	github.com/magefile/mage v1.8.0
 	github.com/pkg/errors v0.9.1
--- logging/gin_logger.go
+++ logging/gin_logger.go
@@ -18,90 +18,161 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
 )
 
-// LoggerConfig holds the configuration for the logging middleware
-type LoggerConfig struct {
-	LogRequestBody  bool
-	LogResponseBody bool
-	LogAssets       bool
-	IgnorePaths     []string
+// Rule holds the configuration for the logging middleware
+type Rule struct {
+	Methods []string
+	Pattern string
+	// Request true means ignore request body, false means log it
+	// when combined with Response true, it means ignore the entire request
+	Config *Config
 }
 
-// NewLoggerConfig creates a new LoggerConfig with default settings.
-// Pass in any paths you want to ignore or leave empty to use default paths.
-func NewLoggerConfig(logRequestBody, logResponseBody, logAssets bool, ignorePaths ...string) *LoggerConfig {
-	defaultPaths := []string{"reports", "file", "swagger", "favicon", "static", "photo"}
-	if len(ignorePaths) == 0 {
-		ignorePaths = defaultPaths
-	}
-	return &LoggerConfig{
-		LogRequestBody:  logRequestBody,
-		LogResponseBody: logResponseBody,
-		LogAssets:       logAssets,
-		IgnorePaths:     ignorePaths,
+// Config holds the configuration for the logging middleware
+type Config struct {
+	LogRequestFact     bool
+	LogRequestBody     bool
+	LogRequestHeaders  bool
+	LogResponseFact    bool
+	LogResponseBody    bool
+	LogResponseHeaders bool
+	LogAssets          bool
+	Rules              []Rule
+}
+
+var justFactsRule = JustFacts()
+
+// DefaultRules are the default rules for logging paths
+var DefaultRules = []Rule{
+	{Methods: []string{"*"}, Pattern: `swagger`, Config: &justFactsRule},
+	{Methods: []string{"*"}, Pattern: `favicon`},
+}
+
+// DefaultLogAllConfig is the default config for logging everything
+var DefaultLogAllConfig = Config{
+	LogRequestFact:     true,
+	LogRequestBody:     true,
+	LogRequestHeaders:  true,
+	LogResponseFact:    true,
+	LogResponseBody:    true,
+	LogResponseHeaders: true,
+	Rules:              DefaultRules,
+}
+
+// AllWithRules returns a config that logs everything with the given rules
+func AllWithRules(rules ...Rule) Config {
+	config := DefaultLogAllConfig
+	config.Rules = append(config.Rules, rules...)
+
+	return config
+}
+
+// JustFacts returns a config that only logs request and response facts
+func JustFacts() Config {
+	return Config{
+		LogRequestFact: false,
+		// response fact includes request fact
+		LogResponseFact: true,
 	}
 }
 
-func Middleware(config *LoggerConfig) gin.HandlerFunc {
+// DefaultIgnoreRules are the default rules for ignoring paths
+var DefaultIgnoreRules = []Rule{
+	{Methods: []string{"GET"}, Pattern: `metrics`, Config: nil},
+}
+
+// Middleware is a gin middleware that logs requests and responses
+func Middleware(config Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if ignoreURL(c, config) {
+		cfg := getActiveOrDefaultConfig(c, config)
+		log.Tracef("config: %+v", cfg)
+
+		id := uuid.New()
+		start := time.Now()
+
+		endpointURL := c.Request.URL.Path
+		if c.Request.URL.RawQuery != "" {
+			endpointURL = endpointURL + "?" + c.Request.URL.RawQuery
+		}
+
+		if !cfg.LogAssets && isStaticAsset(c) {
 			c.Next()
 			return
 		}
 
-		// for h, _ := range c.Request.Header {
-		// 	log.Tracef("[RQH] %s: %s", h, c.GetHeader(h))
-		// }
-		start := time.Now()
-		path := c.Request.URL.Path
-		if c.Request.URL.RawQuery != "" {
-			path = path + "?" + c.Request.URL.RawQuery
+		if cfg.LogRequestHeaders {
+			for h, _ := range c.Request.Header {
+				log.Tracef("[REQH] [%s] %s: %s", id, h, c.GetHeader(h))
+			}
 		}
 
-		// get the response
+		if cfg.LogRequestFact {
+			// request: log a proof of request
+			log.Tracef("[REQ] [%s] %s | %s | %s", id, c.Request.Method, endpointURL, c.ClientIP())
+		}
+
+		if cfg.LogRequestBody {
+			// only these methods can contain request body
+			if requestMethodHasBody(c.Request.Method) {
+				requestBodyBytes, err := io.ReadAll(c.Request.Body)
+				body := bytes.TrimSpace(requestBodyBytes)
+				if err == nil && len(body) > 0 {
+					log.Tracef("[REQ] [%s] body: %s", id, string(requestBodyBytes))
+				}
+				// restore the original body to GIN's body reader and return the io.ReadCloser to its original state
+				// for the next middleware or the route handler
+				c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBodyBytes))
+			}
+		}
+
+		// before Next, first part of response body logging.
+		// build a new response writer to have our own copy of the response body
+		// otherwise it would be lost after c.Next reading it
 		responseBodyWriter := &rewrittenBody{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
-		c.Writer = responseBodyWriter
+		if cfg.LogResponseBody {
+			c.Writer = responseBodyWriter
+		}
+
+		// process the request
 		c.Next()
 
 		statusCode := c.Writer.Status()
-		latency := time.Now().Sub(start)
 
-		// for h, _ := range c.Writer.Header() {
-		// 	log.Tracef("[RSH] %s: %s", h, c.GetHeader(h))
-		// }
-		// log a proof of request
-		log.Tracef("[GIN] %d | %s | %s | %s | %s", statusCode, c.Request.Method, path, latency, c.ClientIP())
+		if cfg.LogResponseFact {
+			latency := time.Now().Sub(start)
 
-		// don't log request body when configured
-		if !config.LogRequestBody {
-			c.Next()
-			return
+			// request: log a proof of request
+			log.Tracef("[RES] [%s] %d | %s | %s | %s | %s", id, statusCode, c.Request.Method, endpointURL, latency, c.ClientIP())
 		}
 
-		// only these methods can contain request body
-		if requestMethodHasBody(c.Request.Method) {
-			requestBodyBytes, err := io.ReadAll(c.Request.Body)
-			if err == nil && len(bytes.TrimSpace(requestBodyBytes)) > 0 {
-				log.Tracef("[REQ] body: %s", string(requestBodyBytes))
+		// after Next, second part of response body logging
+		if cfg.LogResponseBody {
+			// don't log response body when configured
+			// or when the status code is not OK
+			if statusCode >= http.StatusOK && statusCode < http.StatusNoContent {
+				res := responseBodyWriter.body.String()
+				// don't log empty responses
+				// don't log html responses
+				if res != "null" && res != "" && !strings.Contains(res, "<head") {
+					log.Tracef("[RES] [%s] body: %s", id, res)
+				}
 			}
-			// restore the original body to GIN's body reader and return the io.ReadCloser to its original state
-			// for the next middleware
-			c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBodyBytes))
-		}
 
-		if config.LogResponseBody && statusCode >= http.StatusOK && statusCode < http.StatusNoContent {
-			s := responseBodyWriter.body.String()
-			if s != "null" && s != "" && !strings.Contains(s, "<head") {
-				log.Tracef("[RES] body: %s", s)
+			// print request error
+			ginErr := c.Errors.ByType(gin.ErrorTypePrivate).String()
+			if ginErr != "" {
+				log.Errorf("[RES] [%s] %s", id, ginErr)
 			}
 		}
 
-		// print request error
-		ginErr := c.Errors.ByType(gin.ErrorTypePrivate).String()
-		if ginErr != "" {
-			log.Errorf("[ERRz] %s", ginErr)
+		// purposely after response fact for better readability
+		if cfg.LogResponseHeaders {
+			for h, _ := range c.Writer.Header() {
+				log.Tracef("[RESH] [%s] %s: %s", id, h, c.GetHeader(h))
+			}
 		}
 	}
 }
@@ -120,24 +191,39 @@ func (w rewrittenBody) Write(b []byte) (int, error) {
 	return w.ResponseWriter.Write(b)
 }
 
-// ignoreURL checks if the URL should be ignored based on the provided paths
-func ignoreURL(c *gin.Context, config *LoggerConfig) bool {
-	for _, path := range config.IgnorePaths {
-		if strings.Contains(c.Request.URL.Path, path) {
+func getActiveOrDefaultConfig(c *gin.Context, config Config) Config {
+	for _, rule := range config.Rules {
+		matched, err := regexp.MatchString(rule.Pattern, c.Request.URL.Path)
+		if err != nil {
+			log.Errorf("error matching pattern: %v", err)
+			continue
+		}
+		if matched && methodAllowed(c.Request.Method, rule.Methods) {
+			// rule matched, but has no config
+			if rule.Config == nil {
+				// everything false
+				return Config{}
+			}
+			return *rule.Config
+		}
+	}
+
+	return config
+}
+
+func methodAllowed(given string, methods []string) bool {
+	for _, method := range methods {
+		if method == given || method == "*" {
 			return true
 		}
 	}
-	if !config.LogAssets && isStaticAsset(c) {
-		return true
-	}
-
 	return false
 }
 
 // isStaticAsset checks if the request is for a static asset.
 func isStaticAsset(c *gin.Context) bool {
 	// Regular expression to match static file patterns
-	staticAssetPattern := regexp.MustCompile(`\.(css|js|jpg|jpeg|png|gif|svg|ico|woff2|ttf|eot|html)$`)
+	staticAssetPattern := regexp.MustCompile(`\.(css|js|jpg|jpeg|png|gif|svg|ico|woff2|ttf|eot|html|favicon)$`)
 
 	// Check if the request path matches the static asset pattern
 	return staticAssetPattern.MatchString(c.Request.URL.Path)
